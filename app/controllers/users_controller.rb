72df89f079916b632eb4d2cfc0e3ea78a54b393b
Thomas Dupre <thomasdpr-pro@outlook.com>
2023-06-27 11:29:45 +0200

    users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
   def show
+    @user = User.find(params[:id]) # Ça permet de récupérer l'utilisateur en fonction de l'id passé en paramètre dans l'URL
   end
 end
+
+# Ce controller permet de récupérer l'utilisateur en fonction de l'id passé en paramètre dans l'URL.
+# On peut ensuite utiliser cet utilisateur dans la view show.html.erb
